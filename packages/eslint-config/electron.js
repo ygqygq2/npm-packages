00453dffa686e37e3c21e423278bcd6a7d9f4d50
ygqygq2 <ygqygq2@qq.com>
2024-03-27 17:06:23 +0800

    chore: update config
    Co-authored-by: ygqygq2 <ygqygq2@qq.com>

--- packages/eslint-config/electron.js
+++ packages/eslint-config/electron.js
@@ -1,41 +1,39 @@
+const { resolve } = require('node:path');
+
+const project = resolve(process.cwd(), 'tsconfig.json');
+
+/*
+ * This is a custom ESLint configuration for use with
+ * internal (bundled by their consumer) libraries
+ * that utilize React.
+ *
+ * This config extends the Vercel Engineering Style Guide.
+ * For more information, see https://github.com/vercel/style-guide
+ *
+ */
+
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
   extends: [
-    // typescript的eslint插件
-    // https://github.com/typescript-eslint/typescript-eslint/blob/master/docs/getting-started/linting/README.md
-    // https://github.com/typescript-eslint/typescript-eslint/tree/master/packages/eslint-plugin
-    'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    './base',
-  ],
+    '@vercel/style-guide/eslint/browser',
+    '@vercel/style-guide/eslint/typescript',
+    '@vercel/style-guide/eslint/react',
+  ].map(require.resolve),
+  parserOptions: {
+    project,
+  },
+  globals: {
+    JSX: true,
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {
+        project,
+      },
+    },
+  },
+  ignorePatterns: ['node_modules/', 'dist/', '.eslintrc.js'],
+
   rules: {
-    /* ********************************** Typescript ********************************** */
-    '@typescript-eslint/no-unused-vars': 0,
-    '@typescript-eslint/no-empty-interface': 0,
-    '@typescript-eslint/no-this-alias': 0,
-    '@typescript-eslint/no-var-requires': 0,
-    '@typescript-eslint/no-use-before-define': 0,
-    '@typescript-eslint/explicit-member-accessibility': 0,
-    '@typescript-eslint/no-non-null-assertion': 0,
-    '@typescript-eslint/no-unnecessary-type-assertion': 0,
-    '@typescript-eslint/require-await': 0,
-    '@typescript-eslint/no-for-in-array': 0,
-    '@typescript-eslint/interface-name-prefix': 0,
-    '@typescript-eslint/explicit-function-return-type': 0,
-    '@typescript-eslint/no-explicit-any': 0,
-    '@typescript-eslint/explicit-module-boundary-types': 0,
-    '@typescript-eslint/no-floating-promises': 0,
-    '@typescript-eslint/restrict-template-expressions': 0,
-    '@typescript-eslint/no-unsafe-assignment': 0,
-    '@typescript-eslint/no-unsafe-return': 0,
-    '@typescript-eslint/no-unused-expressions': 0,
-    '@typescript-eslint/no-misused-promises': 0,
-    '@typescript-eslint/no-unsafe-member-access': 0,
-    '@typescript-eslint/no-unsafe-call': 0,
-    '@typescript-eslint/no-unsafe-argument': 0,
-    '@typescript-eslint/ban-ts-comment': 0,
-    'no-redundant-type-constituents': 0,
-    '@typescript-eslint/unbound-method': 0,
+    // add specific rules configurations here
   },
 };
